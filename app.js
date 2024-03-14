d3b1edf1897959594a2b45a0ef8449231a3cc86e
Angel Serrato <serratoarias@yahoo.com>
2024-03-13 21:36:02 -0500

    Create functions

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('Hello World');
+function mayus(event) {
+    event.value = event.value.toLowerCase();
+}
+
+function encriptarTexto() {
+    let texto = document.getElementById("input-group__one").value;
+    let outputText = "";
+    for (let i = 0; i < texto.length; i++) {
+        charCode = texto[i];
+        if (charCode == "e") {
+            charCode = charCode + "nter";
+        } else if (charCode == "i") {
+            charCode = charCode + "mes";
+        } else if (charCode == "a") {
+            charCode = charCode + "i";
+        } else if (charCode == "o") {
+            charCode = charCode + "ber";
+        } else if (charCode == "u") {
+            charCode = charCode + "fat";
+        }
+        outputText = outputText + charCode;
+    }
+    document.getElementById("input-group__two").value = outputText;
+}
+
+function desencriptarTexto() {
+    console.log('hola');
+}
